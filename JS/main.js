b6931b023c71c002b49691f9a62fa2b111605082
manana <teresafullstack@gmail.com>
2018-02-20 12:44:45 +0100

    burger

---
+++ JS/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    //Menú hamburguesa
+    $(".burger").click(function(){
+        $(".menu").animate({width:"toggle"},800);
+    });
+    $(".cerrar").click(function(){
+        $(".menu").animate({width:"toggle"},800);
+    });
+    //Menú hamburgesa páginas normales
+    $(".burger2").click(function(){
+        $(".burger2").toggleClass("abierto");
+        $(".menu2").animate({width:"toggle"},800);
+        $(".menu2 a").toggleClass("aparicion");
+    });
+    //Scroll suave
+$("a").on("click", function(){
+    if (this.hash !== ""){
+        var hash = this.hash;
+        $("html, body").animate({
+            //Objeto con propiedad y valor
+            scrollTop: $(hash).offset().top
+        },800);
+    }
+    });
+});
